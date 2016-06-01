75261091d49e0a5cc1a1e8cc5b449a16963c1141
jkepple2003 <jkepple2003@gmail.com>
2016-05-31 21:19:37 -0500

    add arduino robot code.
    Testing how to upload files.

---
+++ motor_test_with_new_driver.ino
@@ -0,0 +1,14 @@
+#include <AFMotor.h>
+#include "adafruit_motor_driver.h"
+#define MOTOR_INIT 1
+
+Michelino::Motor motor(MOTOR_INIT);
+
+void setup()
+{
+    motor.setSpeed(255);
+}
+
+void loop()
+{
+}
